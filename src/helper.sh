1ac85951fd646fd9a2673b71fdd15c1f0cb9c420
Pahud <pahudnet@gmail.com>
2021-03-17 10:36:13 +0800

    add helper script

---
+++ src/helper.sh
@@ -0,0 +1,18 @@
+# ecs_exec_service CLUSTER SERVICE CONTAINER
+function ecs_exec_service() {
+  CLUSTER=$1
+  SERVICE=$2
+  CONTAINER=$3
+  TASK=$(awsv1 ecs list-tasks --service-name $SERVICE --cluster $CLUSTER --query 'taskArns[0]' --output text)
+  ecs_exec_task $CLUSTER $TASK $CONTAINER
+}
+
+# ecs_exec_task CLUSTER TASK CONTAINER
+function ecs_exec_task() {
+  aws ecs execute-command  \
+      --cluster $1 \
+      --task $2 \
+      --container $3 \
+      --command "/bin/bash" \
+      --interactive
+}
