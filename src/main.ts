4ee8569350fb6966c91618e28fbf3c1c1ded2f8a
Pahud <pahudnet@gmail.com>
2021-03-19 17:14:18 +0800

    feat: add security headers extension

--- src/main.ts
+++ src/main.ts
@@ -113,7 +113,7 @@ app.synth();
 
 function getOrCreateVpc(scope: Construct): ec2.IVpc {
   // use an existing vpc or create a new one
-  return scope.node.tryGetContext('use_default_vpc') === '1' 
+  return scope.node.tryGetContext('use_default_vpc') === '1'
     || process.env.CDK_USE_DEFAULT_VPC === '1' ? ec2.Vpc.fromLookup(scope, 'Vpc', { isDefault: true }) :
     scope.node.tryGetContext('use_vpc_id') ?
       ec2.Vpc.fromLookup(scope, 'Vpc', { vpcId: scope.node.tryGetContext('use_vpc_id') }) :
