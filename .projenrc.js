a4ed1f7e76b602ecbc94a23345ed572e09d71162
Pahud <pahudnet@gmail.com>
2021-03-19 09:13:35 +0800

    chore: bump versions

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,7 @@
 const { AwsCdkTypeScriptApp } = require('projen');
+const { Automation } = require('projen-automate-it');
+
+const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
 
 const project = new AwsCdkTypeScriptApp({
   cdkVersion: '1.73.0',
@@ -14,6 +17,17 @@ const project = new AwsCdkTypeScriptApp({
     '@aws-cdk/aws-logs',
     '@aws-cdk/aws-s3',
   ],
+  devDeps: ['projen-automate-it'],
 });
+
+const automation = new Automation(project, {
+  automationToken: AUTOMATION_TOKEN,
+});
+
+automation.projenYarnUpgrade();
+
+const common_exclude = ['cdk.out', 'cdk.context.json', 'images', 'yarn-error.log', 'dependabot.yml'];
+project.npmignore.exclude(...common_exclude);
+project.gitignore.exclude(...common_exclude);
 
 project.synth();
